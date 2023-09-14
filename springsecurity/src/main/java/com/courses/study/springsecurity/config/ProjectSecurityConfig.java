3c1d07799310a8622adb90d653785c4d45a491b8
Leonardo Cruz <cruz.leong95@gmail.com>
2023-09-14 17:54:53 -0500

    Cors and Csfr

--- springsecurity/src/main/java/com/courses/study/springsecurity/config/ProjectSecurityConfig.java
+++ springsecurity/src/main/java/com/courses/study/springsecurity/config/ProjectSecurityConfig.java
@@ -1,45 +1,53 @@
 package com.courses.study.springsecurity.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.courses.study.springsecurity.config.filter.CsrfCookieFilter;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class ProjectSecurityConfig {
 
-    @Autowired
-    UserDetailsService customUserDetailsService;
-
-//    @Bean
-//    public DaoAuthenticationProvider authProvider() {
-//        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-//        authenticationProvider.setPasswordEncoder(passwordEncoder());
-//        authenticationProvider.setUserDetailsService(customUserDetailsService);
-//        return authenticationProvider;
-//    }
-
     @Bean
     @Order(SecurityProperties.BASIC_AUTH_ORDER)
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        return http.csrf(AbstractHttpConfigurer::disable)
+        CsrfTokenRequestAttributeHandler requestAttributeHandler = new CsrfTokenRequestAttributeHandler();
+        requestAttributeHandler.setCsrfRequestAttributeName("_csrf");
+
+        return http.securityContext(securityContextConfigurer -> {
+                    securityContextConfigurer.requireExplicitSave(false);
+                })
+                .sessionManagement(sessionManagement -> {
+                    sessionManagement.sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
+
+                })
+                .csrf(csrf -> {
+                    csrf.csrfTokenRequestHandler(requestAttributeHandler).ignoringRequestMatchers("/contact", "/register")
+                            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+                })
+                .cors(httpSecurityCorsConfigurer ->
+                        httpSecurityCorsConfigurer.configurationSource(corsConfiguration()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests((requests) ->
-                        requests.requestMatchers("my-**").authenticated()
-                                .requestMatchers("register").permitAll()
+                        requests.requestMatchers("/my-**").authenticated()
+                                .requestMatchers("/register", "/contact", "/notices").permitAll()
                 )
                 .formLogin(withDefaults())
                 .httpBasic(withDefaults())
@@ -48,26 +56,18 @@ public class ProjectSecurityConfig {
     }
 
     @Bean
+    public UrlBasedCorsConfigurationSource corsConfiguration() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
-
-//@Configuration
-//class Another {
-//
-//    @Autowired
-//    CustomAuthenticationProvider customAuthenticationProvider;
-//
-//    @Autowired
-//    DaoAuthenticationProvider daoAuthenticationProvider;
-//
-//    @Bean
-//    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
-//        AuthenticationManagerBuilder authenticationManagerBuilder =
-//                http.getSharedObject(AuthenticationManagerBuilder.class);
-//        authenticationManagerBuilder.authenticationProvider(daoAuthenticationProvider);
-//        authenticationManagerBuilder.authenticationProvider(customAuthenticationProvider);
-//        return authenticationManagerBuilder.build();
-//    }
-//}
