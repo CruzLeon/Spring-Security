a632c934cba483c327bbd787084cc608b633898a
Leonardo Cruz <cruz.leong95@gmail.com>
2023-09-20 19:44:34 -0500

    OAUTH2 RESOURCE SERVER

--- springsecurity/src/main/java/com/courses/study/springsecurity/config/ProjectSecurityConfig.java
+++ springsecurity/src/main/java/com/courses/study/springsecurity/config/ProjectSecurityConfig.java
@@ -1,41 +1,30 @@
 package com.courses.study.springsecurity.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
-                .oauth2Login(withDefaults());
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConverter());
+
+        http.sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+                .authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
+                .oauth2ResourceServer(resourceServerConfigurer ->
+                        resourceServerConfigurer.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter)));
 
         return http.build();
-    }
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        ClientRegistration clientRegistration = clientRegistration();
-        return new InMemoryClientRegistrationRepository(clientRegistration);
-    }
-
-    private ClientRegistration clientRegistration() {
-        return
-                CommonOAuth2Provider.GITHUB.getBuilder("github")
-                        .clientId("4628af15e47297b66091")
-                        .clientSecret("192d9b80d7ef34f5d1d1e03c6a6a3a8a2acf0356")
-                        .build();
     }
 
 
--- springsecurity/src/main/java/com/courses/study/springsecurity/controller/ServiceController.java
+++ springsecurity/src/main/java/com/courses/study/springsecurity/controller/ServiceController.java
@@ -1,15 +1,14 @@
 package com.courses.study.springsecurity.controller;
 
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ServiceController {
 
     @GetMapping("/")
-    public String main(OAuth2AuthenticationToken token) {
-        System.out.println(token.getPrincipal());
-        return "secure.html";
+    public String main(@RequestParam String email) {
+        return "secure returned data";
     }
 }
